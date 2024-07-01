924d2421915c2a9e73fe4b58c2e66c198dde817c
Sergio  Martinez <sergiotechx@yahoo.com>
2024-06-30 23:24:05 -0500

    second

---
+++ web/src/config.js
@@ -0,0 +1,7 @@
+const contractPerNetwork = {
+  mainnet: 'hello.near-examples.near',
+  testnet: 'holamundo.nearcolombiadev.testnet',
+};
+
+export const NetworkId = 'testnet';
+export const HelloNearContract = contractPerNetwork[NetworkId];
